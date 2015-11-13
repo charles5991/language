6f986599c09537c1bc9249263e559feaf840970b
m516 <m516@m516>
2015-11-13 16:27:10 +0800

    deleted init,replaced category

--- language/wiki/views.py
+++ language/wiki/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from wiki.forms import CategoryForm
 from wiki.models import Category, Page
 
 
@@ -9,3 +11,25 @@ def wiki(request):
 
 def about(request):
     return render(request, 'wiki/about.html')
+
+def category(request, categoryName):
+    context = {}
+    try:
+        category = Category.objects.get(name=categoryName)
+        context['category'] = category
+        context['pages'] = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'wiki/category.html', context)
+
+def addCategory(request):
+    template = 'wiki/addCategory.html'
+    if request.method=='GET':
+        return render(request, template, {'form':CategoryForm()})
+    # request.method=='POST'
+    form = CategoryForm(request.POST)
+    if not form.is_valid():
+        return render(request, template, {'form':form})
+    form.save()
+    return redirect(reverse('wiki:wiki'))
+    # Or try this: return wiki(request) 
