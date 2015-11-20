b9f04e7241a435c3abe84f880c5da6db42145e40
m516 <m516@m516>
2015-11-20 16:32:33 +0800

    addpage .form. delete step

--- language/wiki/forms.py
+++ language/wiki/forms.py
@@ -10,9 +10,19 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name', )
-    class PageForm(forms.ModelForm):
-        title = forms.CharField(max_length=128, label='頁面標題', help_text='請輸入頁面標題')
-        url = forms.URLField(max_length=128, label='頁面網址', help_text='請輸入頁面網址')
-        class Meta:
-            model = Page
-            exclude = ('category', 'views')
+        
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, label='頁面標題', help_text='請輸入頁面標題')
+    url = forms.URLField(max_length=128, label='頁面網址', help_text='請輸入頁面網址')
+        
+    class Meta:
+        model = Page
+        exclude = ('category', 'views')
+            
+    def clean(self):
+        cleanedData = self.cleaned_data
+        url = cleanedData.get('url')
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleanedData['url'] = url
+        return cleanedData
